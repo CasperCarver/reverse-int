2c57a37541d697d41b8a5c67ee4bd2340e26e022
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-21 22:28:16 +0300

    feat: add solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n<0){
+        n*=-1;
+    }
+    return (n.toString().split('').reverse().join(''));
 }
+
+
+// let n=-123;
+// console.log(Number(n.toString().split('').reverse().join('')));
